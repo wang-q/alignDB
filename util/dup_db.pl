71bf6fb63596442b9ada4fa46e012494a10e740d
wang-q <wang-q@outlook.com>
2015-07-11 15:59:45 +0800

    dup_db.pl + gzip

--- util/dup_db.pl
+++ util/dup_db.pl
@@ -1,11 +1,14 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use autodie;
 
 use Getopt::Long;
 use Pod::Usage;
 use Config::Tiny;
 use YAML qw(Dump Load DumpFile LoadFile);
+
+use IPC::Cmd qw(can_run);
 
 use FindBin;
 
@@ -34,6 +37,8 @@ my $db       = $Config->{database}{db};
 my $goal_db;
 my $file_dump;
 
+my $gzip;
+
 my $man  = 0;
 my $help = 0;
 
@@ -47,10 +52,42 @@ GetOptions(
     'p|password=s' => \$password,
     'f|file=s'     => \$file_dump,
     'g|goal=s'     => \$goal_db,
+    'z|gzip'       => \$gzip,
 ) or pod2usage(2);
 
 pod2usage(1) if $help;
 pod2usage( -exitstatus => 0, -verbose => 2 ) if $man;
+
+#----------------------------------------------------------#
+# Check external commands
+#----------------------------------------------------------#
+unless ( can_run('mysql') ) {
+    die "[mysql] not found in your system.\n";
+}
+
+if ($file_dump) {
+    if ( $file_dump =~ /\.gz$/ ) {
+        if ( !$gzip ) {
+            print
+                "The file [$file_dump] seems like a gzipped file, set --gzip ON.";
+            $gzip++;
+        }
+    }
+    else {
+        if ($gzip) {
+            print "The name of file [$file_dump] doesn't end with '.gz'.\n";
+            print "Append '.gz' to it.\n";
+            $file_dump += '.gz';
+            print "It's [$file_dump] now.\n";
+        }
+    }
+}
+
+if ($gzip) {
+    unless ( can_run('gzip') ) {
+        die "[gzip] not found in your system.\n";
+    }
+}
 
 #----------------------------------------------------------#
 # call mysql
@@ -62,8 +99,15 @@ my $str = " -h$server -P$port -u$username -p$password ";
 # dump db
 if ($file_dump) {
     if ( !-e $file_dump ) {
-        my $dump = "mysqldump $str $db > $file_dump";
-        print "#dump\n$dump\n\n";
+        my $dump;
+        if ($gzip) {
+            $dump = "mysqldump $str $db | gzip > $file_dump";
+        }
+        else {
+            $dump = "mysqldump $str $db  > $file_dump";
+        }
+
+        print "===> dump\n$dump\n\n";
         system($dump);
     }
     else {
@@ -72,16 +116,33 @@ if ($file_dump) {
 }
 else {
     $file_dump = $db . ".dump.sql";
-    my $dump = "mysqldump $str $db > $file_dump";
-    print "#dump\n$dump\n\n";
+    $file_dump += '.gz' if $gzip;
+
+    my $dump;
+    if ($gzip) {
+        $dump = "mysqldump $str $db | gzip > $file_dump";
+    }
+    else {
+        $dump = "mysqldump $str $db  > $file_dump";
+    }
+
+    print "===> dump\n$dump\n\n";
     system($dump);
 }
 
 # load dump
 if ($goal_db) {
-    my $drop      = "mysql $str -e \"DROP DATABASE IF EXISTS $goal_db;\"";
-    my $create    = "mysql $str -e \"CREATE DATABASE $goal_db;\"";
-    my $duplicate = "mysql $str $goal_db < $file_dump";
+    my $drop   = "mysql $str -e \"DROP DATABASE IF EXISTS $goal_db;\"";
+    my $create = "mysql $str -e \"CREATE DATABASE $goal_db;\"";
+    my $duplicate;
+
+    if ($gzip) {
+        $duplicate
+            = "gzip --stdout --decompress --force $file_dump | mysql $str $goal_db";
+    }
+    else {
+        $duplicate = "mysql $str $goal_db < $file_dump";
+    }
 
     print "#drop\n$drop\n\n";
     system($drop);
@@ -97,11 +158,11 @@ __END__
 
 =head1 NAME
 
-    dup_db.pl - Duplicate a database
+dup_db.pl - Duplicate a database or dump to a gzipped file
 
 =head1 SYNOPSIS
 
-    dup_db.pl [options]
+    perl dup_db.pl [options]
       Options:
         --help              brief help message
         --man               full documentation
@@ -112,24 +173,6 @@ __END__
         --password          password
         --file              dump file name
         --goal              dup db name
-
-=head1 OPTIONS
-
-=over 8
-
-=item B<-help>
-
-Print a brief help message and exits.
-
-=item B<-man>
-
-Prints the manual page and exits.
-
-=back
-
-=head1 DESCRIPTION
-
-B<This program> will read the given input file(s) and do someting
-useful with the contents thereof.
+        --gzip              dump file is gzipped
 
 =cut
