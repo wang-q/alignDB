c8db4362f8df0e20a53ea8f5d1aa4f01f07993d9
wang-q <wang-q@outlook.com>
2015-07-15 17:58:46 +0800

    Avoid use mysql's local infile
    This require custom building parameters.

--- init/init_alignDB.pl
+++ init/init_alignDB.pl
@@ -1,11 +1,14 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use autodie;
 
 use Getopt::Long;
 use Pod::Usage;
 use Config::Tiny;
 use YAML qw(Dump Load DumpFile LoadFile);
+
+use Text::CSV_XS;
 
 use FindBin;
 use lib "$FindBin::Bin/../lib";
@@ -93,18 +96,28 @@ my $dbh = $obj->dbh;
 #----------------------------------------------------------#
 # taxon
 #----------------------------------------------------------#
-if (-f $init_taxon) {
+if ( -f $init_taxon ) {
     print "Use $init_taxon to Init table taxon\n";
 
-    my $sql_string = qq{
-        LOAD DATA LOCAL INFILE '$init_taxon'
-        INTO TABLE taxon
-        FIELDS TERMINATED BY ','
-        IGNORE 1 LINES
-    };
-    my $load_sth = $dbh->prepare($sql_string);
-    $load_sth->execute;
-    $load_sth->finish;
+    my $insert_sth = $dbh->prepare(
+        'INSERT INTO taxon (
+            taxon_id, genus, species, sub_species, common_name
+        )
+        VALUES (
+            ?, ?, ?, ?, ? 
+        )'
+    );
+
+    my $csv = Text::CSV_XS->new( { binary => 1, eol => "\n" } );
+    open my $csv_fh, "<", $init_taxon;
+    $csv->getline($csv_fh);    # bypass title line
+
+    while ( my $row = $csv->getline($csv_fh) ) {
+        $insert_sth->execute( $row->[0], $row->[1], $row->[2], $row->[3],
+            $row->[4], );
+    }
+    close $csv_fh;
+    $insert_sth->finish;
 
     # Now retrieve data from the table.
     my $query_sth = $dbh->prepare(
@@ -122,23 +135,33 @@ if (-f $init_taxon) {
             $ref->{genus}, $ref->{species}, $ref->{common_name};
     }
     $query_sth->finish;
+
+    print "\n";
 }
 
 #----------------------------------------------------------#
 # chromosome
 #----------------------------------------------------------#
-if (-f $init_chr) {
+if ( -f $init_chr ) {
     print "Use $init_chr to Init table chromosome\n";
 
-    my $sql_string = qq{
-        LOAD DATA LOCAL INFILE '$init_chr'
-        INTO TABLE chromosome
-        FIELDS TERMINATED BY ','
-        IGNORE 1 LINES
-        (taxon_id, chr_name, chr_length)
-    };
-    my $insert_sth = $dbh->prepare($sql_string);
-    $insert_sth->execute;
+    my $insert_sth = $dbh->prepare(
+        'INSERT INTO chromosome (
+            chr_id, taxon_id, chr_name, chr_length
+        )
+        VALUES (
+            NULL, ?, ?, ?
+        )'
+    );
+
+    my $csv = Text::CSV_XS->new( { binary => 1, eol => "\n" } );
+    open my $csv_fh, "<", $init_chr;
+    $csv->getline($csv_fh);    # bypass title line
+
+    while ( my $row = $csv->getline($csv_fh) ) {
+        $insert_sth->execute( $row->[0], $row->[1], $row->[2], );
+    }
+    close $csv_fh;
     $insert_sth->finish;
 
     # Now retrieve data from the table
@@ -151,7 +174,7 @@ if (-f $init_chr) {
     $query_sth->execute;
     while ( my $ref = $query_sth->fetchrow_hashref ) {
         my $taxon_id = $ref->{taxon_id};
-        if ( defined $taxon_id and $taxon_id == 31033 ) {
+        if ( defined $taxon_id and $taxon_id == 3702 ) {
             printf "Found a row: taxon_id = %s,"
                 . " chr_name = %s, chr_length = %s\n",
                 $ref->{taxon_id}, $ref->{chr_name}, $ref->{chr_length};
@@ -177,11 +200,11 @@ __END__
 
 =head1 NAME
 
-    init_alignDB.pl - Initiate alignDB
+init_alignDB.pl - Initiate alignDB
 
 =head1 SYNOPSIS
 
-    init_alignDB.pl [options]
+    perl init_alignDB.pl [options]
       Options:
         --help          brief help message
         --man           full documentation
@@ -192,23 +215,6 @@ __END__
         --password      password
         --init_sql      init sql filename
 
-=head1 OPTIONS
-
-=over 8
-
-=item B<-help>
-
-Print a brief help message and exits.
-
-=item B<-man>
-
-Prints the manual page and exits.
-
-=back
-
-=head1 DESCRIPTION
-
-B<This program> will read the given input file(s) and do someting
-useful with the contents thereof.
+    perl init_alignDB.pl -d S288cvsYJM789
 
 =cut
