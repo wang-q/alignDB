b524194b9612f9ae89f3652b2b91e05d3a887de2
wang-q <wang-q@outlook.com>
2015-07-29 00:38:38 +0800

    Misc changes

--- util/merge_csv.pl
+++ util/merge_csv.pl
@@ -17,6 +17,7 @@ my $file_target;
 my $file_merge;
 
 my @fields;
+my @fields2;
 
 my $concat;
 my $stdout;
@@ -30,6 +31,7 @@ GetOptions(
     't|ft=s'     => \$file_target,
     'm|fm=s'     => \$file_merge,
     'f|fields=s' => \@fields,
+    'f2|fields2=s' => \@fields2,
     'concat'     => \$concat,
     'stdout'     => \$stdout,
 ) or pod2usage(2);
@@ -58,7 +60,14 @@ my $line_m = [];
     while (<$fh>) {
         chomp;
         next unless $_;
-        push @{$id_m}, join( "_", ( split /,/ )[@fields] );
+        my $id;
+        if ($concat and scalar @fields2) {
+            $id = join( "_", ( split /,/ )[@fields2] );
+        }
+        else {
+            $id = join( "_", ( split /,/ )[@fields] );
+        }
+        push @{$id_m}, $id;
         push @{$line_m}, $_;
         $count_m++;
     }
@@ -133,13 +142,14 @@ merge_csv.pl - Merge two csv files based on @fields
 
     merge_csv.pl [options]
       Options:
-        --help              brief help message
-        --man               full documentation
-        -t, --ft STR        target file (output)
-        -m, --fm SRT        merge file
-        -f, --fields @INT   fields as identifies
-        --concat            do concat other than merge
-        --stdout            write outputs to stdout instead of the target file
+        --help                  brief help message
+        --man                   full documentation
+        -t, --ft STR            target file (output)
+        -m, --fm SRT            merge file
+        -f, --fields @INT       fields as identifies
+        --concat                do concat other than merge
+        -f2, --fieldss @INT     fields in concat file
+        --stdout                write outputs to stdout instead of the target file
 
     perl merge_csv.pl -t ../init/taxon.csv -m d:\data\alignment\yeast_combine\taxon.csv 
     perl merge_csv.pl -t ../init/chr_length.csv -m d:\data\alignment\yeast_combine\chr_length.csv
