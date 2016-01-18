216a43c9a473c99a9ac5606af7cd66c270c0da53
wang-q <wang-q@outlook.com>
2016-01-19 01:42:25 +0800

    New style

--- util/dup_db.pl
+++ util/dup_db.pl
@@ -3,22 +3,19 @@ use strict;
 use warnings;
 use autodie;
 
-use Getopt::Long;
-use Pod::Usage;
+use Getopt::Long qw(HelpMessage);
 use Config::Tiny;
+use FindBin;
 use YAML qw(Dump Load DumpFile LoadFile);
 
 use IPC::Cmd qw(can_run);
-
-use FindBin;
 
 use AlignDB::Stopwatch;
 
 #----------------------------------------------------------#
 # GetOpt section
 #----------------------------------------------------------#
-my $Config = Config::Tiny->new;
-$Config = Config::Tiny->read("$FindBin::Bin/../alignDB.ini");
+my $Config = Config::Tiny->read("$FindBin::RealBin/../alignDB.ini");
 
 # record ARGV and Config
 my $stopwatch = AlignDB::Stopwatch->new(
@@ -27,49 +24,52 @@ my $stopwatch = AlignDB::Stopwatch->new(
     program_conf => $Config,
 );
 
-# Database init values
-my $server   = $Config->{database}{server};
-my $port     = $Config->{database}{port};
-my $username = $Config->{database}{username};
-my $password = $Config->{database}{password};
-my $db       = $Config->{database}{db};
+=head1 NAME
 
-my $goal_db;
-my $file_dump;
+dup_db.pl - Duplicate a database or dump to a gzipped file
 
-my $gzip;
+=head1 SYNOPSIS
 
-my $man  = 0;
-my $help = 0;
+    perl dup_db.pl [options]
+      Options:
+        --help      -?          brief help message
+        --server    -s  STR     MySQL server IP/Domain name
+        --port      -P  INT     MySQL server port
+        --db        -d  STR     database name
+        --username  -u  STR     username
+        --password  -p  STR     password
+        --file                  dump file name
+        --goal                  dup db name
+        --gzip                  dump file is gzipped
+
+    perl dup_db.pl -d S288CvsRM11 -z # S288CvsRM11.dump.sql.gz
+    perl dup_db.pl -f S288CvsRM11.dump.sql.gz -g S288CvsRM11_dup
+    
+=cut
 
 GetOptions(
-    'help|?'       => \$help,
-    'man'          => \$man,
-    's|server=s'   => \$server,
-    'P|port=i'     => \$port,
-    'd|db=s'       => \$db,
-    'u|username=s' => \$username,
-    'p|password=s' => \$password,
-    'f|file=s'     => \$file_dump,
-    'g|goal=s'     => \$goal_db,
-    'z|gzip'       => \$gzip,
-) or pod2usage(2);
-
-pod2usage(1) if $help;
-pod2usage( -exitstatus => 0, -verbose => 2 ) if $man;
+    'help|?' => sub { HelpMessage(0) },
+    'server|s=s'   => \( my $server   = $Config->{database}{server} ),
+    'port|P=i'     => \( my $port     = $Config->{database}{port} ),
+    'db|d=s'       => \( my $db       = $Config->{database}{db} ),
+    'username|u=s' => \( my $username = $Config->{database}{username} ),
+    'password|p=s' => \( my $password = $Config->{database}{password} ),
+    'file|f=s'     => \( my $file_dump ),
+    'goal|g=s'     => \( my $goal_db ),
+    'gzip|z'       => \( my $gzip ),
+) or HelpMessage(1);
 
 #----------------------------------------------------------#
 # Check external commands
 #----------------------------------------------------------#
-unless ( can_run('mysql') ) {
+if ( !can_run('mysql') ) {
     die "[mysql] not found in your system.\n";
 }
 
 if ($file_dump) {
     if ( $file_dump =~ /\.gz$/ ) {
         if ( !$gzip ) {
-            print
-                "The file [$file_dump] seems like a gzipped file, set --gzip ON.\n";
+            print "The file [$file_dump] seems like a gzipped file, set --gzip ON.\n";
             $gzip++;
         }
     }
@@ -137,8 +137,7 @@ if ($goal_db) {
     my $duplicate;
 
     if ($gzip) {
-        $duplicate
-            = "gzip --stdout --decompress --force $file_dump | mysql $str $goal_db";
+        $duplicate = "gzip --stdout --decompress --force $file_dump | mysql $str $goal_db";
     }
     else {
         $duplicate = "mysql $str $goal_db < $file_dump";
@@ -155,27 +154,3 @@ if ($goal_db) {
 $stopwatch->end_message;
 
 __END__
-
-=head1 NAME
-
-dup_db.pl - Duplicate a database or dump to a gzipped file
-
-=head1 SYNOPSIS
-
-    perl dup_db.pl [options]
-      Options:
-        --help              brief help message
-        --man               full documentation
-        --server            MySQL server IP/Domain name
-        --port              MySQL server port
-        --db                database name
-        --username          username
-        --password          password
-        --file              dump file name
-        --goal              dup db name
-        --gzip              dump file is gzipped
-
-    perl dup_db.pl -d S288CvsRM11 -z # S288CvsRM11.dump.sql.gz
-    perl dup_db.pl -f S288CvsRM11.dump.sql.gz -g S288CvsRM11_dup
-    
-=cut
