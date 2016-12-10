d8a9d880c401a372e7b44ea34c9a6b0e3009f9d9
wang-q <wang-q@outlook.com>
2016-12-10 23:17:08 +0800

    Switch to Getopt::Long::Descriptive and MCE

--- init/gen_alignDB_genome.pl
+++ init/gen_alignDB_genome.pl
@@ -3,16 +3,16 @@ use strict;
 use warnings;
 use autodie;
 
-use Getopt::Long;
+use Getopt::Long::Descriptive;
 use Config::Tiny;
 use FindBin;
 use YAML::Syck;
 
 use File::Find::Rule;
+use MCE;
 use Path::Tiny;
 
 use AlignDB::IntSpan;
-use AlignDB::Run;
 use AlignDB::Stopwatch;
 
 use App::Fasops::Common;
@@ -23,81 +23,86 @@ use AlignDB;
 #----------------------------------------------------------#
 # GetOpt section
 #----------------------------------------------------------#
-my $Config = Config::Tiny->read("$FindBin::RealBin/../alignDB.ini");
+my $conf = Config::Tiny->read("$FindBin::RealBin/../alignDB.ini");
 
-# record ARGV and Config
-my $stopwatch = AlignDB::Stopwatch->new(
-    program_name => $0,
-    program_argv => [@ARGV],
-    program_conf => $Config,
-);
+# record command line
+my $stopwatch = AlignDB::Stopwatch->new->record;
 
-=head1 NAME
+my $description = <<'EOF';
+Generate alignDB from genomic fasta files
 
-gen_alignDB_genome.pl - Generate alignDB from genome fasta files
+    perl init/gen_alignDB_genome.pl -d S288Cvsself --parallel 2 \
+        -t S288c --da ~/data/alignment/example/scer/Genomes/S288c
 
-=head1 SYNOPSIS
+Usage: perl %c [options]
+EOF
 
-    perl gen_alignDB_genome.pl [options]
-      Options:
-        --help          -?              brief help message
-        --server        -s      STR     MySQL server IP/Domain name
-        --port          -P      INT     MySQL server port
-        --db            -d      STR     database name
-        --username      -u      STR     username
-        --password      -p      STR     password
-        --dir_align     -da     STR     fasta files' directory
-        --target                STR     target_name
-        --length                INT     truncated length
-        --fill                  INT     fill holes less than this
-        --min                   INT     minimal length
-        --parallel              INT     run in parallel mode
+(
+    #@type Getopt::Long::Descriptive::Opts
+    my $opt,
 
-=cut
+    #@type Getopt::Long::Descriptive::Usage
+    my $usage,
+    )
+    = Getopt::Long::Descriptive::describe_options(
+    $description,
+    [ 'help|h', 'display this message' ],
+    [],
+    ['Database init values'],
+    [ 'server|s=s',   'MySQL IP/Domain', { default => $conf->{database}{server} }, ],
+    [ 'port=i',       'MySQL port',      { default => $conf->{database}{port} }, ],
+    [ 'username|u=s', 'username',        { default => $conf->{database}{username} }, ],
+    [ 'password|p=s', 'password',        { default => $conf->{database}{password} }, ],
+    [ 'db|d=s',       'database name',   { default => $conf->{database}{db} }, ],
+    [],
+    [ 'dir_align|da=s', 'dir contains .fa files',    { required => 1 }, ],
+    [ 'target|t=s',     'target name',               { required => 1 }, ],
+    [ 'length|lt=i',    'threshold of lengths',      { default  => $conf->{generate}{length} }, ],
+    [ 'truncate=i',     'truncated length',          { default  => 100_000 }, ],
+    [ 'fill=i',         'fill holes less than this', { default  => 50 }, ],
+    [ 'parallel=i',     'run in parallel mode',      { default  => $conf->{generate}{parallel} }, ],
+    { show_defaults => 1, }
+    );
 
-GetOptions(
-    'help|?' => sub { Getopt::Long::HelpMessage(0) },
-    'server|s=s'   => \( my $server   = $Config->{database}{server} ),
-    'port|P=i'     => \( my $port     = $Config->{database}{port} ),
-    'db|d=s'       => \( my $db       = $Config->{database}{db} ),
-    'username|u=s' => \( my $username = $Config->{database}{username} ),
-    'password|p=s' => \( my $password = $Config->{database}{password} ),
-    'dir_align|dir|da=s' => \( my $dir ),
-    'target=s'           => \( my $target_name ),
-    'length=i'           => \( my $truncated_length = 100_000 ),
-    'fill=i'             => \( my $fill = 50 ),
-    'min=i'              => \( my $min_length = 5000 ),
-    'parallel=i'         => \( my $parallel = $Config->{generate}{parallel} ),
-) or Getopt::Long::HelpMessage(1);
+$usage->die if $opt->{help};
+
+# record config
+$stopwatch->record_conf($opt);
+
+# DBI Data Source Name
+my $dsn = sprintf "dbi:mysql:database=%s;host=%s;port=%s", $opt->{db}, $opt->{server}, $opt->{port};
+
+die "target_name not defined\n" unless $opt->{target};
 
 #----------------------------------------------------------#
 # Search for all files and push their paths to @axt_files
 #----------------------------------------------------------#
-my @files = sort File::Find::Rule->file->name( '*.fa', '*.fas', '*.fasta' )->in($dir);
+$opt->{dir_align} = path( $opt->{dir_align} )->stringify;
+my @files
+    = sort File::Find::Rule->file->name( '*.fa', '*.fas', '*.fasta' )->in( $opt->{dir_align} );
 printf "\n----Total .fa Files: %4s----\n\n", scalar @files;
 
 #----------------------------------------------------------#
 # worker
 #----------------------------------------------------------#
 my $worker = sub {
-    my $infile = shift;
+    my ( $self, $chunk_ref, $chunk_id ) = @_;
+    my $infile = $chunk_ref->[0];
+    my $wid    = MCE->wid;
 
-    my $inner_watch = AlignDB::Stopwatch->new;
-    $inner_watch->block_message("Process $infile...");
+    my $inner    = AlignDB::Stopwatch->new;
+    my $basename = path($infile)->basename;
+    $inner->block_message("Process task [$chunk_id] by worker #$wid. [$basename]");
 
-    my $obj = AlignDB->new(
-        mysql  => "$db:$server",
-        user   => $username,
-        passwd => $password,
+    my $alignDB = AlignDB->new(
+        dsn    => $dsn,
+        user   => $opt->{username},
+        passwd => $opt->{password},
     );
-
-    die "target_name not defined\n" unless $target_name;
 
     my $chr_name = path($infile)->basename( '.fasta', '.fas', '.fa' );
 
     my $seq_of = App::Fasops::Common::read_fasta($infile);
-    printf "File [%s] loaded\n", $infile;
-
     my $chr_seq = $seq_of->{ ( keys %{$seq_of} )[0] };
     my $chr_length = length $chr_seq;
 
@@ -113,21 +118,21 @@ my $worker = sub {
 
     my $valid_set = AlignDB::IntSpan->new("1-$chr_length");
     $valid_set->subtract($ambiguous_set);
-    $valid_set = $valid_set->fill( $fill - 1 );    # fill gaps smaller than $fill
+    $valid_set = $valid_set->fill( $opt->{fill} - 1 );    # fill small gaps
 
     printf "Valid chromosome region for [%s]:\n    %s\n", $chr_name, $valid_set->runlist;
 
-    my @regions;                                   # ([start, end], [start, end], ...)
+    my @regions;                                          # ([start, end], [start, end], ...)
     for my AlignDB::IntSpan $set ( $valid_set->sets ) {
         my $size = $set->size;
-        next if $size < $min_length;
+        next if $size < $opt->{length};
 
         my @set_regions;
         my $pos = $set->min;
         my $max = $set->max;
-        while ( $max - $pos + 1 > $truncated_length ) {
-            push @set_regions, [ $pos, $pos + $truncated_length - 1 ];
-            $pos += $truncated_length;
+        while ( $max - $pos + 1 > $opt->{truncate} ) {
+            push @set_regions, [ $pos, $pos + $opt->{truncate} - 1 ];
+            $pos += $opt->{truncate};
         }
         if ( scalar @set_regions > 0 ) {
             $set_regions[-1]->[1] = $max;
@@ -145,51 +150,44 @@ my $worker = sub {
         my $seq = substr $chr_seq, $start - 1, $end - $start + 1;
 
         my $info_refs = [
-            {   name       => $target_name,
-                chr_name   => $chr_name,
-                chr_start  => $start,
-                chr_end    => $end,
-                chr_strand => '+',
-                seq        => $seq,
+            {   name   => $opt->{target},
+                chr    => $chr_name,
+                start  => $start,
+                end    => $end,
+                strand => '+',
+                seq    => $seq,
             },
-            {   name       => $target_name,
-                chr_name   => $chr_name,
-                chr_start  => $start,
-                chr_end    => $end,
-                chr_strand => '+',
-                seq        => $seq,
+            {   name   => $opt->{target},
+                chr    => $chr_name,
+                start  => $start,
+                end    => $end,
+                strand => '+',
+                seq    => $seq,
             },
         ];
 
-        $obj->add_align($info_refs);
+        $alignDB->add_align($info_refs);
     }
 
-    $inner_watch->block_message( "$infile has been processed.", "duration" );
+    $inner->block_message( "[$basename] has been processed.", "duration" );
 
     return;
 };
 
 #----------------------------------------------------------#
-# start
+# start insert
 #----------------------------------------------------------#
-my $run = AlignDB::Run->new(
-    parallel => $parallel,
-    jobs     => \@files,
-    code     => $worker,
-);
-$run->run;
+my $mce = MCE->new( max_workers => $opt->{parallel}, chunk_size => 1, );
+$mce->foreach( [ sort @files ], $worker );    # foreach also implies chunk_size => 1
 
 $stopwatch->end_message( "All files have been processed.", "duration" );
 
-# store program running meta info to database
-# this AlignDB object is just for storing meta info
-END {
-    AlignDB->new(
-        mysql  => "$db:$server",
-        user   => $username,
-        passwd => $password,
-    )->add_meta_stopwatch($stopwatch);
-}
+# store program's meta info to database
+AlignDB->new(
+    dsn    => $dsn,
+    user   => $opt->{username},
+    passwd => $opt->{password},
+)->add_meta_stopwatch($stopwatch);
 
 exit;
 
